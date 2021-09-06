284cb98f7a5f4c21de9dc6469f8cb2b914c6c785
hehmde <hehmde@ukr.net>
2021-09-06 20:43:41 +0300

    initial commit

---
+++ src/test/java/ua/hehmde/kvjewelry/KvJewelryApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.hehmde.kvjewelry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvJewelryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
